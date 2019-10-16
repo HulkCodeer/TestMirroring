1866b61d93e21e1b46c3a0f66c1b78720bab9914
sosopark <jspark@soft-berry.com>
2019-10-16 14:45:27 +0900

    Smartro Web Page 분리작업중

---
+++ evInfra/ViewController/member/MyPayRegisterViewController.swift
@@ -0,0 +1,30 @@
+//
+//  MyPayRegisterViewController.swift
+//  evInfra
+//
+//  Created by bulacode on 16/10/2019.
+//  Copyright © 2019 soft-berry. All rights reserved.
+//
+
+import UIKit
+
+class MyPayRegisterViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
