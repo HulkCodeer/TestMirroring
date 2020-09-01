6eaaf716d2aa1fc7316501fa5fa81b9e851fa1e1
shin.park0321 <shin.park0321@gmail.com>
2020-09-01 16:25:26 +0900

    마커 생성 확인 후 지도에 마커 그리도록 체크

--- evInfra/charger/ChargerManager.swift
+++ evInfra/charger/ChargerManager.swift
@@ -8,89 +8,85 @@
 
 import Foundation
 import SwiftyJSON
+
 class ChargerManager {
     
     static let sharedInstance = ChargerManager()
     
-    private var mDb : DataBaseHelper? = nil;
+    private var mDb: DataBaseHelper? = nil
 
-    private var mIsReady : Bool = false
+    private var mIsReady: Bool = false
 
     private var mChargerStationInfoList = [ChargerStationInfo]()
 
-    public init(){
-        
-        if (Const.EV_PAY_SERVER.contains("https") == false){
+    public init() {
+        if Const.EV_PAY_SERVER.contains("https") == false {
             DataBaseHelper.DATABASE_NAME = DataBaseHelper.DEV_DATABASE_NAME
         }
     
-        if (DataBaseHelper.importDatabase()){
-            Log.d(tag: Const.TAG, msg: "im success");
-        }else{
-            Log.d(tag: Const.TAG, msg: "im fail");
+        if DataBaseHelper.importDatabase() {
+            Log.d(tag: Const.TAG, msg: "im success")
+        } else {
+            Log.d(tag: Const.TAG, msg: "im fail")
         }
         mDb = DataBaseHelper.sharedInstance
     }
     
-    public func isReady() -> Bool{
+    public func isReady() -> Bool {
         return mIsReady
     }
 
     // charger company
-    public func getCompanyInfoListAll() -> [CompanyInfoDto]?{
+    public func getCompanyInfoListAll() -> [CompanyInfoDto]? {
         return try! mDb?.getCompanyInfoList()
     }
     
-    public func getCompanyNameList() -> [String]{
+    public func getCompanyNameList() -> [String] {
         var companyNameList = [String]()
         companyNameList.append("전체선택")
 
-        for company in getCompanyInfoListAll()!{
-            if (StringUtils.isNullOrEmpty(company.name!) == false){
+        for company in getCompanyInfoListAll()! {
+            if StringUtils.isNullOrEmpty(company.name!) == false {
                 companyNameList.append(company.name!)
             }
         }
-        
+
         return companyNameList
     }
 
-    public func getCompanyHomePageUrl(companyID : String) -> String?{
-        let companyInfo = try! mDb!.getCompanyInfo(company_id: companyID)
-        if (companyInfo != nil){
-            return companyInfo!.homepage
+    public func getCompanyHomePageUrl(companyID : String) -> String? {
+        if let companyInfo = try! mDb!.getCompanyInfo(company_id: companyID) {
+            return companyInfo.homepage
         }
         return nil
     }
 
-    public func getCompanyMarketUrl(companyID : String) -> String?{
-        let companyInfo = try! mDb!.getCompanyInfo(company_id: companyID)
-        if (companyInfo != nil){
-            return companyInfo!.market
+    public func getCompanyMarketUrl(companyID : String) -> String? {
+        if let companyInfo = try! mDb!.getCompanyInfo(company_id: companyID) {
+            return companyInfo.market
         }
         return nil
     }
 
-    public func getCompanyName(companyID : String) -> String?{
-        let companyInfo = try! mDb!.getCompanyInfo(company_id: companyID)
-        if (companyInfo != nil){
-            return companyInfo!.name
+    public func getCompanyName(companyID : String) -> String? {
+        if let companyInfo = try! mDb!.getCompanyInfo(company_id: companyID) {
+            return companyInfo.name
         }
         return nil
     }
     
-    public func getCompanyId(companyName : String) -> String?{
-        let companyInfo = try! mDb!.getCompanyInfo(company_name: companyName)
-        if (companyInfo != nil){
-            return companyInfo!.company_id
+    public func getCompanyId(companyName : String) -> String? {
+        if let companyInfo = try! mDb!.getCompanyInfo(company_name: companyName) {
+            return companyInfo.company_id
         }
         return nil
     }
     
-    public func getCompanyVisibilityList() -> [Bool]{
+    public func getCompanyVisibilityList() -> [Bool] {
         var companyVisibleList = [Bool]()
         companyVisibleList.append(true)
 
-        for company in getCompanyInfoListAll()!{
+        for company in getCompanyInfoListAll()! {
             if company.is_visible {
                 companyVisibleList.append(true)
             } else {
@@ -102,35 +98,29 @@ class ChargerManager {
         return companyVisibleList
     }
 
-    private func updateCompanyInfoListFromServer(json : JSON) -> Bool{
+    private func updateCompanyInfoListFromServer(json : JSON) {
         let code = json["code"]
         let list = json["list"]
         let last = json["last"]
 
         if (code == 1000 && list.array!.count > 0) {
-            
             var companyList = [CompanyInfoDto]()
             
-            for company in list.arrayValue{
+            for company in list.arrayValue {
                 let companyInfo = CompanyInfoDto()
                 companyInfo.setCompanyInfo(json: company)
                 companyList.append(companyInfo)
             }
             
-            try! mDb?.insertOrUpdateCompanyInfoList(list: companyList);
-            try! mDb?.insertOrUpdateInfoLastUpdate(info_type: ChargerConst.INFO_TYPE_COMPANY, lastUpdate: last.stringValue);
-
-            return false;
-        } else {
-            return true;
+            try! mDb?.insertOrUpdateCompanyInfoList(list: companyList)
+            try! mDb?.insertOrUpdateInfoLastUpdate(info_type: ChargerConst.INFO_TYPE_COMPANY, lastUpdate: last.stringValue)
         }
     }
 
     public func updateCompanyVisibility(isVisible : Bool, companyID : String) {
-        let companyInfo = try! mDb?.getCompanyInfo(company_id: companyID)!
-        if (companyInfo != nil) {
-            companyInfo?.is_visible = isVisible
-            try! mDb?.updateCompanyInfo(companyInfo: companyInfo!)
+        if let companyInfo = try! mDb?.getCompanyInfo(company_id: companyID)! {
+            companyInfo.is_visible = isVisible
+            try! mDb?.updateCompanyInfo(companyInfo: companyInfo)
         }
     }
 
@@ -141,7 +131,7 @@ class ChargerManager {
         let last = json["last"]
         
         if (code == 1000 && list.array!.count > 0) {
-            Log.d(tag: Const.TAG, msg: "start station info list insert or update");
+            Log.d(tag: Const.TAG, msg: "start station info list insert or update")
             
             var stationList = [StationInfoDto]()
             
@@ -151,9 +141,9 @@ class ChargerManager {
                 stationList.append(stationInfo)
             }
             
-            try! mDb?.insertOrUpdateStationInfoList(list: stationList);
-            try! mDb?.insertOrUpdateInfoLastUpdate(info_type: ChargerConst.INFO_TYPE_STATION,lastUpdate: last.stringValue);
-            Log.d(tag: Const.TAG, msg: "end station info list insert or update");
+            try! mDb?.insertOrUpdateStationInfoList(list: stationList)
+            try! mDb?.insertOrUpdateInfoLastUpdate(info_type: ChargerConst.INFO_TYPE_STATION,lastUpdate: last.stringValue)
+            Log.d(tag: Const.TAG, msg: "end station info list insert or update")
         }
     }
 
@@ -161,16 +151,16 @@ class ChargerManager {
         try! mDb?.updateStationInfo(stationInfo: stationInfo)
     }
 
-    public func getStationInfoListAll() -> [StationInfoDto]?{
+    public func getStationInfoListAll() -> [StationInfoDto]? {
         return try! mDb?.getStationInfoList()
     }
 
-    public func getStationInfoById(id : String) -> StationInfoDto?{
+    public func getStationInfoById(id : String) -> StationInfoDto? {
         return try! mDb?.getStationInfoById(id: id)
     }
 
     private func createAllMarker() {
-        for  chargerStationInfo in self.mChargerStationInfoList {
+        for chargerStationInfo in self.mChargerStationInfoList {
             chargerStationInfo.createMarker()
         }
         Log.d(tag: Const.TAG, msg: "marker create")
@@ -180,13 +170,13 @@ class ChargerManager {
     private func setChargerStationInfoList() {
         mChargerStationInfoList.removeAll()
 
-        let stationInfoDtoList = getStationInfoListAll();
+        let stationInfoDtoList = getStationInfoListAll()
         for stationInfoDto in stationInfoDtoList! {
             mChargerStationInfoList.append(ChargerStationInfo(stationInfoDto))
         }
     }
 
-    public func getChargerStationInfoList() -> [ChargerStationInfo]{
+    public func getChargerStationInfoList() -> [ChargerStationInfo] {
         return self.mChargerStationInfoList
     }
     
@@ -212,43 +202,36 @@ class ChargerManager {
         }
     }
 
-    public func getChargerStationInfoById(charger_id : String) -> ChargerStationInfo?{
-        let chargerStationInfo = ChargerStationInfo(charger_id);
+    public func getChargerStationInfoById(charger_id : String) -> ChargerStationInfo? {
+        let chargerStationInfo = ChargerStationInfo(charger_id)
         if let searchIndex = binarySearch(mChargerStationInfoList, chargerStationInfo) {
             if (searchIndex >= 0 && searchIndex < mChargerStationInfoList.count) {
-                return mChargerStationInfoList[searchIndex];
+                return mChargerStationInfoList[searchIndex]
             } else {
-                Log.e(tag: Const.TAG, msg: "charger list size: \(mChargerStationInfoList.count), charger_id: \(charger_id), index: \(searchIndex)");
+                Log.e(tag: Const.TAG, msg: "charger list size: \(mChargerStationInfoList.count), charger_id: \(charger_id), index: \(searchIndex)")
             }
         }
-        return nil;
+        return nil
     }
 
     // from server or from db
     public func getChargerCompanyInfo(listener : ChargerManagerListener?) {
-        var dto : InfoLastUpdateDto? = nil;
         var updateDate = ""
         if (try! (mDb?.getCompanyInfoList()!.count)! > 0) {
-            dto = try! mDb?.getCompanyInfoLastUpdate()
-            if (dto != nil){
-                updateDate = (dto?.mInfoLastUpdateDate)!
+            if let dto = try! mDb?.getCompanyInfoLastUpdate() {
+                updateDate = (dto.mInfoLastUpdateDate)!
             }
         }
         
         Server.getCompanyInfo(updateDate: updateDate) { (isSuccess, value) in
             if isSuccess {
-                if (self.updateCompanyInfoListFromServer(json: JSON(value))){
-                    if (listener != nil) {
-                        listener!.onComplete();
-                    }
-                }else{
-                    if (listener != nil) {
-                        listener!.onComplete();
-                    }
+                self.updateCompanyInfoListFromServer(json: JSON(value))
+                if let chargerManagerListener = listener {
+                    chargerManagerListener.onComplete()
                 }
-            }else{
-                if (listener != nil) {
-                    listener!.onError(errorMsg: "network error");
+            } else {
+                if let chargerManagerListener = listener {
+                    chargerManagerListener.onError(errorMsg: "network error")
                 }
             }
         }
@@ -256,25 +239,25 @@ class ChargerManager {
 
     public func getStationInfoFromServer(listener : ChargerManagerListener?) {
         var updateDate = ""
-        let dto = try! mDb?.getStationInfoLastUpdate()
-        if (dto != nil){
-            updateDate = (dto?.mInfoLastUpdateDate)!
+        if let dto = try! mDb?.getStationInfoLastUpdate() {
+            updateDate = (dto.mInfoLastUpdateDate)!
         }
+
         Server.getStationInfo(updateDate: updateDate) { (isSuccess, value) in
             if isSuccess {
                 /*
-                 var stationInfoTask = StationInfoTask(response, listener);
-                 stationInfoTask.execute();
+                 var stationInfoTask = StationInfoTask(response, listener)
+                 stationInfoTask.execute()
                  */
-                self.updateStationInfoListFromServer(json: JSON(value!));
-                self.setChargerStationInfoList();
-                self.getStationStatus(listener: listener);
+                self.updateStationInfoListFromServer(json: JSON(value!))
+                self.setChargerStationInfoList()
+                self.getStationStatus(listener: listener)
             }
         }
     }
 
     private func getStationStatus(listener : ChargerManagerListener?) {
-        Log.d(tag: Const.TAG, msg: "station status start")
+
         Server.getStationStatus { (isSuccess, value) in
             if isSuccess {
                 let json = JSON(value)
@@ -283,25 +266,23 @@ class ChargerManager {
 
                 if (code == 1000 && list.count > 0) {
                 
-                    for status in list.arrayValue{
+                    for status in list.arrayValue {
                         let chargerId = status["id"].stringValue
                         let cst = status["st"].intValue
                         let power = status["p"].intValue
                         let type_id = status["tp"].intValue
                         
-                        let chargerStationInfo = self.getChargerStationInfoById(charger_id: chargerId);
-                        if (chargerStationInfo != nil) {
-                            chargerStationInfo!.mTotalStatus = cst
-                            chargerStationInfo!.mTotalType = type_id
-                            chargerStationInfo!.mPower = power
+                        if let chargerStationInfo = self.getChargerStationInfoById(charger_id: chargerId) {
+                            chargerStationInfo.mTotalStatus = cst
+                            chargerStationInfo.mTotalType = type_id
+                            chargerStationInfo.mPower = power
                         }
                     }
-                    
-                    Log.d(tag: Const.TAG, msg: "station status end");
-                    self.createAllMarker();
+
+                    self.createAllMarker()
                     self.mIsReady = true
-                    if (listener != nil) {
-                        listener!.onComplete()
+                    if let chargerManagerListener = listener {
+                        chargerManagerListener.onComplete()
                     }
                 }
             }
@@ -309,6 +290,7 @@ class ChargerManager {
     }
 
     public func getFavoriteList(listener : ChargerManagerListener?) {
+        
         Server.getFavoriteList { (isSuccess, value) in
             if isSuccess {
                 let json = JSON(value)
@@ -317,45 +299,45 @@ class ChargerManager {
                 
                 if (code == 1000 && list.count > 0) {
                 
-                    for favorite in list.arrayValue{
+                    for favorite in list.arrayValue {
                         let chargerId = favorite["id"].stringValue
                         let noti = favorite["noti"].boolValue
                         
-                        let chargerStationInfo = self.getChargerStationInfoById(charger_id: chargerId);
-                        if (chargerStationInfo != nil) {
-                            chargerStationInfo!.mFavorite = true
-                            chargerStationInfo!.mFavoriteNoti = noti
+                        if let chargerStationInfo = self.getChargerStationInfoById(charger_id: chargerId) {
+                            chargerStationInfo.mFavorite = true
+                            chargerStationInfo.mFavoriteNoti = noti
                         }
                     }
                     
-                    if (listener != nil) {
-                        listener!.onComplete();
+                    if let chargerManagerListener = listener {
+                        chargerManagerListener.onComplete()
                     }
                 }
             }
         }
     }
+
 /*
     public class StationInfoTask extends AsyncTask<Void, Boolean, Boolean> {
-        ChargerManager.ChargerManagerListener listener;
-        String json;
+        ChargerManager.ChargerManagerListener listener
+        String json
 
         StationInfoTask(String json, ChargerManagerListener listener) {
-            this.json = json;
-            this.listener = listener;
+            this.json = json
+            this.listener = listener
         }
 
         @Override
         protected Boolean doInBackground(Void... voids) {
-            updateStationInfoListFromServer(this.json);
-            setChargerStationInfoList();
-            return true;
+            updateStationInfoListFromServer(this.json)
+            setChargerStationInfoList()
+            return true
         }
 
         @Override
         protected void onPostExecute(Boolean b) {
-            super.onPostExecute(b);
-            getStationStatus(this.listener);
+            super.onPostExecute(b)
+            getStationStatus(this.listener)
         }
     }
  */
@@ -398,34 +380,33 @@ class ChargerManager {
     // sk open api
     public func findAllPOI(keyword : String, callback : FindAllPOIListenerCallback) {
         if let currentPosition = MainViewController.currentLocation{
-            findAllPOI(centerLat: currentPosition.getLatitude(), centerLon: currentPosition.getLongitude(), keyword: keyword, callback: callback);
+            findAllPOI(centerLat: currentPosition.getLatitude(), centerLon: currentPosition.getLongitude(), keyword: keyword, callback: callback)
         }
     }
 
     public func findAllPOI(centerLat : Double, centerLon : Double, keyword : String, callback : FindAllPOIListenerCallback) {
         
-        if (StringUtils.isNullOrEmpty(keyword)){
+        if StringUtils.isNullOrEmpty(keyword) {
             callback.onFindAllPOI(poiList: nil)
         }
         
         Server.getPoiItemList(count: 100, radius: 0, centerLat: centerLat, centerLon: centerLon, keyword: keyword) { (isSuccess, result) in
-            if (isSuccess){
+            if  isSuccess {
                 let json = JSON(result)
                 let searchPoiInfo = json["searchPoiInfo"]
-                let totalCount = searchPoiInfo["totalCount"].intValue
-                let count = searchPoiInfo["count"].intValue
-                let page = searchPoiInfo["page"].intValue
+                _ = searchPoiInfo["totalCount"].intValue
+                _ = searchPoiInfo["count"].intValue
+                _ = searchPoiInfo["page"].intValue
                 let pois = searchPoiInfo["pois"]
                 let poi = pois["poi"]
                 var poiList = [EIPOIItem].deserialize(from: poi.rawString())
                 
-                if ((poiList) != nil){
-                    
+                if (poiList) != nil {
                     poiList!.sort(by: { (first, second) -> Bool in
-                        if ((first == nil) || (second == nil)){
+                        if (first == nil) || (second == nil) {
                             return false
                         }
-                        if let currentPosition = MainViewController.currentLocation{
+                        if let currentPosition = MainViewController.currentLocation {
                             let t1 = currentPosition.getDistanceWith(first!.getPOIPoint())
                             let t2 = currentPosition.getDistanceWith(second!.getPOIPoint())
                             return t1 < t2
@@ -433,10 +414,9 @@ class ChargerManager {
                         return false
                     })
                     callback.onFindAllPOI(poiList: (poiList as! [EIPOIItem]))
-                }else{
+                } else {
                     callback.onFindAllPOI(poiList: nil)
                 }
-        
             }
         }
     }
