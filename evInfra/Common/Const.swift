802515eb2afb8866c3e2894e69131a86ad5084a6
SooJin Choi <>
2021-07-21 14:08:35 +0900

    Const) 바뀐 개발서버 적용

--- evInfra/Common/Const.swift
+++ evInfra/Common/Const.swift
@@ -25,7 +25,7 @@ public class Const {
     /*
      * develop
      */
-    public static let EV_SERVER_IP = "http://13.124.50.142:2025"  // homepage server
+    public static let EV_SERVER_IP = "http://121.124.124.198:2025"  // homepage server
     public static let EV_PAY_SERVER = "http://dev.soft-berry.co.kr"
 //    public static let EV_PAY_SERVER = "http://spark.soft-berry.co.kr"
 //    public static let EV_PAY_SERVER = "http://jspark.soft-berry.co.kr"
