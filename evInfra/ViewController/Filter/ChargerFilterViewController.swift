50172f665991f2ab05ee82ef93e105c27cccf7b5
ssuChoi <shchoi@soft-berry.com>
2021-08-09 15:08:34 +0900

    상세필터 - 액션바 버튼UI

--- evInfra/ViewController/Filter/ChargerFilterViewController.swift
+++ evInfra/ViewController/Filter/ChargerFilterViewController.swift
@@ -17,11 +17,41 @@ class ChargerFilterViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        prepareActionBar()
         initView()
+    }
+    
+    func prepareActionBar() {
+        let backButton = IconButton(image: Icon.cm.arrowBack)
+        backButton.tintColor = UIColor(rgb: 0x15435C)
+        backButton.addTarget(self, action: #selector(handleBackButton), for: .touchUpInside)
+        
+        let logoutButton = UIButton()
+        logoutButton.setTitle("초기화", for: .normal)
+        logoutButton.setTitleColor(UIColor(rgb: 0x15435C), for: .normal)
+        logoutButton.titleLabel?.font = .systemFont(ofSize: 14)
+        logoutButton.addTarget(self, action: #selector(resetFilter), for: .touchUpInside)
+        
+        navigationItem.titleLabel.textColor = UIColor(rgb: 0x15435C)
+        navigationItem.titleLabel.text = "필터설정"
+        navigationItem.leftViews = [backButton]
+        navigationItem.rightViews = [logoutButton]
+        self.navigationController?.isNavigationBarHidden = false
     }
     
     func initView(){
         btnApply.backgroundColor = UIColor(named: "content-positive")
         btnApply.layer.cornerRadius = 6
     }
+    
+    @objc
+    fileprivate func handleBackButton() {
+        self.navigationController?.pop()
+    }
+    
+    @objc
+    fileprivate func resetFilter(){
+        print("필터 초기화")
+    }
+    
 }
