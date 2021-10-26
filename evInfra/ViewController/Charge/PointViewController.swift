ed8a115ccc08c29ee7e6df256d8449bc781475f1
ssuChoi <shchoi@soft-berry.com>
2021-10-26 11:06:11 +0900

    베리내역 소멸예정베리 색상 범위 오류 수정

--- evInfra/ViewController/Charge/PointViewController.swift
+++ evInfra/ViewController/Charge/PointViewController.swift
@@ -210,9 +210,8 @@ extension PointViewController {
                     
                     let currMonth = Calendar.current.component(.month, from: Date())
                     var expireMsg = String(format:"%0d월 소멸예정 베리 ", currMonth)
-                    let expirePoint = "\(self.pointHistory.expire_point)".currency()
+                    let expirePoint = "\(self.pointHistory.expire_point)".currency() + " 베리"
                     expireMsg.append(expirePoint)
-                    expireMsg.append(" 베리")
                     let range = (expireMsg as NSString).range(of: expirePoint)
                     let mutableAttributedString = NSMutableAttributedString.init(string: expireMsg)
                     mutableAttributedString.addAttribute(NSAttributedString.Key.foregroundColor, value: UIColor.black, range: range)
