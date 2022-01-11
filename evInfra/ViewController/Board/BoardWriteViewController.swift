ce024646c5892516b2bcedcf7dc2281eee31215e
PKH <>
2022-01-11 17:03:44 +0900

    게시글 작성 화면 UI 추가 (진행중)

---
+++ evInfra/ViewController/Board/BoardWriteViewController.swift
@@ -0,0 +1,19 @@
+//
+//  BoardWriteViewController.swift
+//  evInfra
+//
+//  Created by PKH on 2022/01/10.
+//  Copyright © 2022 soft-berry. All rights reserved.
+//
+
+import UIKit
+
+class BoardWriteViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        self.navigationItem.titleLabel.text = "글쓰기"
+        // Do any additional setup after loading the view.
+    }
+}
