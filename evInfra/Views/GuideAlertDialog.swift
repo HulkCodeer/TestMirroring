d292214fe7884d5be964417cd99a17177db0520c
suhyun choi <shchoi@soft-berry.com>
2022-04-22 10:04:21 +0900

    webview 에서 deeplink 연결 수정

--- evInfra/Views/GuideAlertDialog.swift
+++ evInfra/Views/GuideAlertDialog.swift
@@ -86,10 +86,13 @@ class GuideAlertDialog: UIView, WKUIDelegate, WKNavigationDelegate {
             if let newURL = navigationAction.request.url,
                 let host = newURL.host , !host.hasPrefix(Const.EV_PAY_SERVER) &&
                 UIApplication.shared.canOpenURL(newURL) {
-                if host.hasPrefix("evinfra.page.link") { // deeplink
+                if host.hasPrefix("com.soft-berry.ev-infra") { // deeplink
                     if #available(iOS 13.0, *) {
                         let sceneDelegate = UIApplication.shared.connectedScenes.first?.delegate as? SceneDelegate
                         DeepLinkPath.sharedInstance.linkPath = newURL.path
+                        if let component = URLComponents(url: newURL, resolvingAgainstBaseURL: false) {
+                            DeepLinkPath.sharedInstance.linkParameter = component.queryItems
+                        }
                         DeepLinkPath.sharedInstance.runDeepLink(navigationController: (sceneDelegate?.navigationController)!)
                     } else {
                         UIApplication.shared.open(newURL, options: [:], completionHandler: nil)
