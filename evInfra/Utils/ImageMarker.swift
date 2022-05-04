bbeb79f689636427b9726d54dec859aa2b839e35
khpark <khpark@soft-berry.com>
2022-05-04 15:37:32 +0900

    필터뷰 > 사업자 아이콘 강제언랩핑 오류 제거

--- evInfra/Utils/ImageMarker.swift
+++ evInfra/Utils/ImageMarker.swift
@@ -77,8 +77,11 @@ public class ImageMarker {
     
     public static func companyImg(company: String) -> UIImage? {
         let eFM = EVFileManager.sharedInstance
-        let companyImage = eFM.getSavedImage(named: "\(company).png")
-        
-        return companyImage
+        if let companyImage = eFM.getSavedImage(named: "\(company).png") {
+            print("company: \(company) \(companyImage)")
+            return companyImage
+        }
+
+        return UIImage(named: "icon_building_sm")
     }
 }
