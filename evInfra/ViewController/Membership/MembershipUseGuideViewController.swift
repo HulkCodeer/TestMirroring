473ff354d8305c45d6f18a8f74f7cc59fc4dfc36
hyunjin <parkhj@soft-berry.com>
2022-05-26 16:44:12 +0900

    :nut_and_bolt: 딥링크로 변경

--- evInfra/ViewController/Membership/MembershipUseGuideViewController.swift
+++ evInfra/ViewController/Membership/MembershipUseGuideViewController.swift
@@ -37,7 +37,7 @@ internal final class MembershipUseGuideViewController: BaseViewController, WKUID
             $0.edges.equalToSuperview()
         }
                                         
-        guard let _url = URL(string: "\(Const.EV_PAY_SERVER)/docs/info/membership_card") else {
+        guard let _url = URL(string: "\(Const.EV_PAY_SERVER)/docs/info/membership_card_i") else {
             return
         }
         let requestUrl = URLRequest(url: _url)
@@ -45,41 +45,41 @@ internal final class MembershipUseGuideViewController: BaseViewController, WKUID
     }
     
     // 추후 딥링크 추가시 필요
-//     func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
-//         if let url = navigationAction.request.url, url.scheme == "evinfra" {
-//             DeepLinkModel.shared.openSchemeURL(navi: navigationController ?? UINavigationController() ,urlstring: url.absoluteString)
-//         }
-//         decisionHandler(.allow)
-//
-//         return
-//     }
+     func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+         if let url = navigationAction.request.url, url.scheme == "evinfra" {
+             DeepLinkModel.shared.openSchemeURL(urlstring: url.absoluteString)
+         }
+         decisionHandler(.allow)
+
+         return
+     }
     
-    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
-        
-        if navigationAction.navigationType == .linkActivated  {
-            if let newURL = navigationAction.request.url,
-                let host = newURL.host, UIApplication.shared.canOpenURL(newURL) {
-                if host.hasPrefix("com.soft-berry.ev-infra") { // deeplink
-                    if #available(iOS 13.0, *) {
-                        DeepLinkPath.sharedInstance.linkPath = newURL.path
-                        if let component = URLComponents(url: newURL, resolvingAgainstBaseURL: false) {
-                            DeepLinkPath.sharedInstance.linkParameter = component.queryItems
-                        }
-                        DeepLinkPath.sharedInstance.runDeepLink()
-                    } else {
-                        UIApplication.shared.open(newURL, options: [:], completionHandler: nil)
-                    }
-                } else {
-                    UIApplication.shared.open(newURL, options: [:], completionHandler: nil)
-                }
-                decisionHandler(.cancel)
-            } else {
-                decisionHandler(.allow)
-            }
-        } else {
-            decisionHandler(.allow)
-        }
-    }
+//    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+//
+//        if navigationAction.navigationType == .linkActivated  {
+//            if let newURL = navigationAction.request.url,
+//                let host = newURL.host, UIApplication.shared.canOpenURL(newURL) {
+//                if host.hasPrefix("com.soft-berry.ev-infra") { // deeplink
+//                    if #available(iOS 13.0, *) {
+//                        DeepLinkPath.sharedInstance.linkPath = newURL.path
+//                        if let component = URLComponents(url: newURL, resolvingAgainstBaseURL: false) {
+//                            DeepLinkPath.sharedInstance.linkParameter = component.queryItems
+//                        }
+//                        DeepLinkPath.sharedInstance.runDeepLink()
+//                    } else {
+//                        UIApplication.shared.open(newURL, options: [:], completionHandler: nil)
+//                    }
+//                } else {
+//                    UIApplication.shared.open(newURL, options: [:], completionHandler: nil)
+//                }
+//                decisionHandler(.cancel)
+//            } else {
+//                decisionHandler(.allow)
+//            }
+//        } else {
+//            decisionHandler(.allow)
+//        }
+//    }
 }
 
 extension MembershipUseGuideViewController: UIWebViewDelegate {
