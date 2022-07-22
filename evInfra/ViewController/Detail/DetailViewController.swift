e0c3a62802eafdb3dbc6b00c51f87c381fe5460f
khpark <khpark@soft-berry.com>
2022-07-22 14:25:55 +0900

    :hammer: 옵저버 등록 코드 viewWillAppear로 이동

--- evInfra/ViewController/Detail/DetailViewController.swift
+++ evInfra/ViewController/Detail/DetailViewController.swift
@@ -89,7 +89,9 @@ internal final class DetailViewController: BaseViewController {
                 self.onClickEditBtn()
             })
             .disposed(by: self.disposeBag)
-        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
         NotificationCenter.default.addObserver(self, selector: #selector(self.updateCompletion(_:)), name: Notification.Name("ReloadData"), object: nil)
     }
     
