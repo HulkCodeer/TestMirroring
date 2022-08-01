ca102770c2621839a7d33b2f2887da925b374c55
khpark <khpark@soft-berry.com>
2022-08-01 15:05:25 +0900

    :hammer: view_enter 프로퍼티 이름 다시 원복 (page -> type)
    안드로이드에서 iOS 기준으로 맞춤

--- evInfra/Utils/Extensions/UIViewController+Extension.swift
+++ evInfra/Utils/Extensions/UIViewController+Extension.swift
@@ -22,7 +22,7 @@ extension UIViewController {
     
     @objc public func viewEnterEventInViewWillAppear() {
         guard let title = self.title else { return }
-        let property: [String: Any] = ["page" : title]
+        let property: [String: Any] = ["type" : title]
         AmplitudeManager.shared.logEvent(type: .enter(.viewEnter), property: property)
     }
 }
