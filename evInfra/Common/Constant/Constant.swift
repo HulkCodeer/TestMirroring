08410251173cc84a89bfb5abc0aaeaf4c37168c8
youjin <yjkim@soft-berry.com>
2022-08-11 19:56:55 +0900

    :seeding: 공용 Constant

---
+++ evInfra/Common/Constant/Constant.swift
@@ -0,0 +1,14 @@
+//
+//  Constant.swift
+//  evInfra
+//
+//  Created by youjin kim on 2022/08/11.
+//  Copyright © 2022 soft-berry. All rights reserved.
+//
+
+import Foundation
+
+enum Constant {
+    static let date = DateConstant.self
+
+}
---
+++ evInfra/Common/Constant/DateConstant.swift
@@ -0,0 +1,20 @@
+//
+//  DateConstant.swift
+//  evInfra
+//
+//  Created by youjin kim on 2022/08/11.
+//  Copyright © 2022 soft-berry. All rights reserved.
+//
+
+import Foundation
+
+enum DateConstant {
+    
+    static let year = "yyyy"
+    static let month = "M"
+    static let day = "dd"
+    static let longDateShortTime = "yyyy.MM.dd HH:mm"
+    static let yearMonthDayHangul = "yyyy년 MM월 dd일"
+    static let monthDayDot = "MM.dd"
+    static let time = "HH:mm"
+}
