8bcf515f9e8fcd32e88930f124f86346efc7db5c
khpark <khpark@soft-berry.com>
2022-08-16 13:58:36 +0900

    :nut_and_bolt: 광고 데이터와 게시물 데이터 순서 보장을 위해 dispatch group 사용

--- evInfra/ViewModel/Board/BoardListViewModel.swift
+++ evInfra/ViewModel/Board/BoardListViewModel.swift
@@ -21,97 +21,104 @@ internal final class BoardListViewModel {
     private var indexOfAd: Int = 0
     private var adList: [BoardListItem] = [BoardListItem]()
     private var categoryType: Board.CommunityType = .FREE
+    private let fetchAdListGroup = DispatchGroup()
     
     internal init(_ type: Board.CommunityType) {
+        fetchAdListGroup.enter()
         let adType: Promotion.Page = Board.CommunityType.convertToEventKey(communityType: type)
         
         EIAdManager.sharedInstance.getAdsList(page: adType, layer: Promotion.Layer.mid) { adsList in
             self.adList = adsList.map { BoardListItem($0) }
+            self.fetchAdListGroup.leave()
         }
     }
     
     func fetchFirstBoard(mid: String, sort: Board.SortType, currentPage: Int, mode: String) {
-        Server.fetchBoardList(mid: mid,
-                              page: "\(currentPage)",
-                              mode: mode,
-                              sort: sort.rawValue,
-                              searchType: "",
-                              searchKeyword: "") { (isSuccess, value) in
-            guard isSuccess else { return }
-            guard let data = value else { return }
-            
-            let decoder = JSONDecoder()
-            
-            do {
-                var result = try decoder.decode(BoardResponseData.self, from: data)
-                let countOfList = result.list!.count
-                var boardIndex = countOfList/2
+        fetchAdListGroup.notify(queue: .global(), execute: {
+            Server.fetchBoardList(mid: mid,
+                                  page: "\(currentPage)",
+                                  mode: mode,
+                                  sort: sort.rawValue,
+                                  searchType: "",
+                                  searchKeyword: "") { (isSuccess, value) in
+                guard isSuccess else { return }
+                guard let data = value else { return }
                 
+                let decoder = JSONDecoder()
                 
-                guard countOfList == 20 else {
-                    self.boardResponseData = result
-                    return
-                }
-                
-                // 광고 게시글 insert
-                if !self.adList.isEmpty {
-                    for _ in [currentPage, currentPage+1] {
-                        result.list?.insert(self.adList[self.indexOfAd], at: boardIndex)
-                        boardIndex = boardIndex + 10
-                        self.indexOfAd += 1
-                        
-                        if self.indexOfAd == self.adList.count {
-                            self.indexOfAd = 0
+                do {
+                    var result = try decoder.decode(BoardResponseData.self, from: data)
+                    let countOfList = result.list!.count
+                    var boardIndex = countOfList/2
+                    
+                    
+                    guard countOfList == 20 else {
+                        self.boardResponseData = result
+                        return
+                    }
+                    
+                    // 광고 게시글 insert
+                    if !self.adList.isEmpty {
+                        for _ in [currentPage, currentPage+1] {
+                            result.list?.insert(self.adList[self.indexOfAd], at: boardIndex)
+                            boardIndex = boardIndex + 10
+                            self.indexOfAd += 1
+                            
+                            if self.indexOfAd == self.adList.count {
+                                self.indexOfAd = 0
+                            }
                         }
                     }
+                    
+                    self.boardResponseData = result
+                } catch {
+                    printLog(out: "error")
                 }
-                
-                self.boardResponseData = result
-            } catch {
-                debugPrint("error")
             }
-        }
+        })
     }
     
     func fetchNextBoard(mid: String, sort: Board.SortType, currentPage: Int, mode: String) {
-        Server.fetchBoardList(mid: mid,
-                              page: "\(currentPage)",
-                              mode: mode,
-                              sort: sort.rawValue,
-                              searchType: "",
-                              searchKeyword: "") { (isSuccess, value) in
-            guard isSuccess else { return }
-            guard let data = value else { return }
-            
-            let decoder = JSONDecoder()
-            
-            do {
-                var result = try decoder.decode(BoardResponseData.self, from: data)
-                let countOfList = result.list!.count
-                var boardIndex = countOfList/2
+        fetchAdListGroup.notify(queue: .global(), execute: {
+            Server.fetchBoardList(mid: mid,
+                                  page: "\(currentPage)",
+                                  mode: mode,
+                                  sort: sort.rawValue,
+                                  searchType: "",
+                                  searchKeyword: "") { (isSuccess, value) in
+                guard isSuccess else { return }
+                guard let data = value else { return }
                 
-                guard countOfList == 20 else {
-                    self.boardResponseData = result
-                    return
-                }
+                let decoder = JSONDecoder()
                 
-                // 광고 게시글 insert
-                if !self.adList.isEmpty {
-                    for _ in [currentPage, currentPage+1] {
-                        result.list?.insert(self.adList[self.indexOfAd], at: boardIndex)
-                        boardIndex = boardIndex + 10
+                do {
+                    var result = try decoder.decode(BoardResponseData.self, from: data)
+                    let countOfList = result.list!.count
+                    var boardIndex = countOfList/2
+                    
+                    guard countOfList == 20 else {
+                        self.boardResponseData = result
+                        return
+                    }
+                    
+                    // 광고 게시글 insert
+                    if !self.adList.isEmpty {
+                        for _ in [currentPage, currentPage+1] {
+                            result.list?.insert(self.adList[self.indexOfAd], at: boardIndex)
+                            boardIndex = boardIndex + 10
 
-                        self.indexOfAd += 1
-                        if self.indexOfAd == self.adList.count {
-                            self.indexOfAd = 0
+                            self.indexOfAd += 1
+                            if self.indexOfAd == self.adList.count {
+                                self.indexOfAd = 0
+                            }
                         }
                     }
-                }
 
-                self.boardResponseData = result
-            } catch {
-                debugPrint("error")
+                    self.boardResponseData = result
+                } catch {
+                    printLog(out: "error")
+                }
             }
-        }
+        })
     }
 }
