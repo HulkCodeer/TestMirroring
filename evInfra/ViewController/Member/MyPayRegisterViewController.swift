9a8fefee27fb33185cd48495b28e5cddee75365d
khpark <khpark@soft-berry.com>
2022-08-29 14:35:04 +0900

    :construction: 앰플리튜드 맴버쉽 영역 이벤트 추가

--- evInfra/ViewController/Member/MyPayRegisterViewController.swift
+++ evInfra/ViewController/Member/MyPayRegisterViewController.swift
@@ -26,6 +26,7 @@ class MyPayRegisterViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        logEvent(with: .clickAddPaymentCard)
         prepareActionBar()
         makePostRequest(url: Const.EV_PAY_SERVER + "/pay/evPay/registEvPay", payload: ["mb_id":"\(MemberManager.shared.mbId)"])
         // Do any additional setup after loading the view.
@@ -175,5 +176,16 @@ extension MyPayRegisterViewController: WKScriptMessageHandler {
     func showRegisteredResult(json: JSON) {
         self.navigationController?.pop()
         myPayRegisterViewDelegate?.finishRegisterResult(json: json)
+    }
+}
+
+// MARK: - Amplitude Loggin 이벤트
+extension MyPayRegisterViewController {
+    private func logEvent(with event: EventType.PaymentEvent) {
+        switch event {
+        case .clickAddPaymentCard:
+            AmplitudeManager.shared.logEvent(type: .payment(event), property: nil)
+        default: break
+        }
     }
 }
