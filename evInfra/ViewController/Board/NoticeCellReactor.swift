62a42d82c9c3e94619b487b75720a9e784454f56
youjin <yjkim@soft-berry.com>
2022-09-08 14:42:12 +0900

    :construction: 공지사항 Cell Reactor

---
+++ evInfra/ViewController/Board/NoticeCellReactor.swift
@@ -0,0 +1,49 @@
+//
+//  NoticeCellReactor.swift
+//  evInfra
+//
+//  Created by youjin kim on 2022/09/07.
+//  Copyright © 2022 soft-berry. All rights reserved.
+//
+
+import Foundation
+import ReactorKit
+
+final class NoticeCellReactor: Reactor {
+    enum Action {
+        case moveDetailView
+    }
+    
+    enum Mutation {
+        case setDetailID(Int)
+    }
+    
+    struct State {
+        let noticeID: Int
+    }
+        
+    internal var initialState: State
+            
+    init(noticeID: Int) {
+        self.initialState = State(noticeID: noticeID)
+    }
+    
+    func mutate(action: Action) -> Observable<Mutation> {
+        switch action {
+        case .moveDetailView:
+            let noticeID = currentState.noticeID
+            
+            let reactor = NoticeDetailReactor(provider: RestApi(), noticeID: noticeID)
+            let detailVC = NewNoticeDetailViewController(reactor: reactor)
+            GlobalDefine.shared.mainNavi?.push(viewController: detailVC)
+            
+            return .empty()
+        }
+    }
+    
+    func reduce(state: State, mutation: Mutation) -> State {
+        let newState = state
+        return newState
+    }
+    
+}
