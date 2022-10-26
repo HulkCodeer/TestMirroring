d34ca1c0872cd804120a753c91e277aff9bffa4c
youjin <yjkim@soft-berry.com>
2022-10-26 11:18:09 +0900

    :construction: MainNavigationBar 코드리뷰 적용

--- evInfra/ViewController/Main/Views/MainNavigationBar.swift
+++ evInfra/ViewController/Main/Views/MainNavigationBar.swift
@@ -36,8 +36,8 @@ internal final class MainNavigationBar: UIView {
         $0.layer.cornerRadius = 8
     }
     private lazy var searchChargeIcon = UIImageView().then {
-        $0.image = UIImage(asset: Icons.iconSearchMd)?
-            .withTintColor(Colors.contentTertiary.color)
+        $0.image = UIImage(asset: Icons.iconSearchMd)
+        $0.tintColor = Colors.contentTertiary.color
         $0.contentMode = .scaleAspectFill
     }
     private lazy var searchChargeText = UILabel().then {
@@ -56,8 +56,8 @@ internal final class MainNavigationBar: UIView {
         $0.imageView?.contentMode = .scaleAspectFill
     }
     internal lazy var cancelSearchWayButton = UIButton().then {
-        let image = UIImage(asset: Icons.iconCloseLg)?
-            .withTintColor(Colors.contentTertiary.color)
+        let image = UIImage(asset: Icons.iconCloseLg)
+        $0.tintColor = Colors.contentTertiary.color
         $0.setImage(image, for: .normal)
         $0.imageView?.contentMode = .scaleAspectFill
         $0.isHidden = true
@@ -73,72 +73,59 @@ internal final class MainNavigationBar: UIView {
     override init(frame: CGRect) {
         super.init(frame: frame)
         
-        setUI()
-        setConstraints()
-    }
-    
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
-    }
-    
-    // MARK: - makeUI
-    
-    private func setUI() {
         self.backgroundColor = Colors.backgroundPrimary.color
         
-        self.addSubview(contentStackView)
-        
-        contentStackView.addArrangedSubview(menuButton)
-        contentStackView.addArrangedSubview(searchChargeContentsView)
-        contentStackView.addArrangedSubview(searchWayContentView)
-        
-        searchChargeContentsView.addSubview(searchChargeIcon)
-        searchChargeContentsView.addSubview(searchChargeText)
-        searchChargeContentsView.addSubview(searchChargeButton)
-        
-        searchWayContentView.addSubview(searchWayButton)
-        searchWayContentView.addSubview(cancelSearchWayButton)
-    }
-    
-    private func setConstraints() {
         let verticalPadding: CGFloat = 8
         let searchChargeInset: CGFloat = 10
         
         let itemSize: CGFloat = 40
         let searchChargeIconSize: CGFloat = 20
         
+        self.addSubview(contentStackView)
         contentStackView.snp.makeConstraints {
             $0.height.equalTo(itemSize)
             $0.leading.trailing.equalToSuperview().inset(verticalPadding)
             $0.bottom.equalToSuperview().inset(4)
         }
         
+        contentStackView.addArrangedSubview(menuButton)
         menuButton.snp.makeConstraints {
             $0.width.equalTo(itemSize)
         }
-
+        contentStackView.addArrangedSubview(searchChargeContentsView)
+        contentStackView.addArrangedSubview(searchWayContentView)
         searchWayContentView.snp.makeConstraints {
             $0.width.equalTo(itemSize)
         }
-        searchWayButton.snp.makeConstraints {
-            $0.edges.equalToSuperview()
-        }
-        cancelSearchWayButton.snp.makeConstraints {
-            $0.edges.equalToSuperview()
-        }
         
+        searchChargeContentsView.addSubview(searchChargeIcon)
         searchChargeIcon.snp.makeConstraints {
             $0.leading.equalToSuperview().offset(searchChargeInset)
             $0.centerY.equalToSuperview()
             $0.size.equalTo(searchChargeIconSize)
         }
+        searchChargeContentsView.addSubview(searchChargeText)
         searchChargeText.snp.makeConstraints {
             $0.leading.equalTo(searchChargeIcon.snp.trailing).offset(searchChargeInset)
             $0.centerY.equalToSuperview()
         }
+        searchChargeContentsView.addSubview(searchChargeButton)
         searchChargeButton.snp.makeConstraints {
             $0.top.leading.trailing.bottom.equalToSuperview()
         }
+        
+        searchWayContentView.addSubview(searchWayButton)
+        searchWayButton.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+        searchWayContentView.addSubview(cancelSearchWayButton)
+        cancelSearchWayButton.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
     }
     
     // MARK: - Action
